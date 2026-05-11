58416f99425e092c9981f2b8f362dd904df29de5
varun-bhatiya <varun.bhatia508@gmail.com>
2026-05-11 17:45:05 +0530

    test

--- C_Dishant_sir/practical/check.c
+++ C_Dishant_sir/practical/check.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int i , j , k;
-    for(i = 1 ; i <= 5 ; i++)
+    int i, j, k;
+    for (i = 1; i <= 5; i++)
     {
-        for(j =  1 ; j <= i ; j++)
+        for (j = 1; j <= i; j++)
         {
             printf("* ");
         }
-        for(k = 10 ; k > i*2; k--)
+        for (k = 10; k > i * 2; k--)
         {
             printf("  ");
         }
-        for(j =  1 ; j <= i ; j++)
+        for (j = 1; j <= i; j++)
         {
             printf("* ");
-        }   
+        }
         printf("\n");
     }
     return 0;

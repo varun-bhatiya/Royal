8540d39e92dbf520ba8b5369d442c3a42cb422ea
varun-bhatiya <varun.bhatia508@gmail.com>
2026-05-13 14:11:08 +0530

    fibo

--- C_Dishant_sir/loops/do_while/fibonacci.c
+++ C_Dishant_sir/loops/do_while/fibonacci.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int i=0,j,p1 = 0,p2 = 1,sum,num;
+    int i = 0, j, p1 = 0, p2 = 1, sum, num;
     printf("Enter the number : ");
-    scanf("%d",&num);
-    while(i <= num)
+    scanf("%d", &num);
+    while (i <= num)
     {
         sum = p1 + p2;
-        printf("%d\t",sum);
+        printf("%d\t", sum);
         p1 = p2;
         p2 = sum;
         i++;

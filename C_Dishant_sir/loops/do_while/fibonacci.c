a1b79be21d80ab9fafcc04d45b9e93dd5e436b08
varun-bhatiya <varun.bhatia508@gmail.com>
2026-05-13 14:12:43 +0530

    fibo

--- C_Dishant_sir/loops/do_while/fibonacci.c
+++ C_Dishant_sir/loops/do_while/fibonacci.c
@@ -5,13 +5,13 @@ int main()
     int i = 0, j, p1 = 0, p2 = 1, sum, num;
     printf("Enter the number : ");
     scanf("%d", &num);
-    while (i <= num)
-    {
+    do{
         sum = p1 + p2;
         printf("%d\t", sum);
         p1 = p2;
         p2 = sum;
         i++;
     }
+    while (i <= num);
     return 0;
 }
